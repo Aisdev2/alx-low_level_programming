97a510eeb4d8483ca093bdac3f6f2f2a67d69800
Aisosa Ogbebor Matthew <aisdev99@gmail.com>
2023-03-19 06:18:30 -0700

    Task on c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,28 @@
+/*
+ * Add header file
+ * print text without using print(3) and puts(3) function in the linux manual
+ * return 1
+ */
+
+/*
+ * using write() to print value
+ * SYNTAX
+ * write(decriptor, string, length of string)
+ */
+
+/**
+ * main - print a string using write()
+ *
+ * Return: 1
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 write(1, &str, 59);
+
+	return (1);
+}
