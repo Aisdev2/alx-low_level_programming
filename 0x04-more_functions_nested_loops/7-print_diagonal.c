c12f40c32166f75c88ca517d2df47177130d60b6
Aisosa Ogbebor Matthew <aisdev99@gmail.com>
2023-03-24 12:30:58 -0700

    Task 7 and more prototype in main.h

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line in terminal
+ * @n: number of time it drawn
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
