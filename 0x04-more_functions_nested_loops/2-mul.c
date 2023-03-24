02ca94260985a14405b894384ef179253efbbc12
Aisosa Ogbebor Matthew <aisdev99@gmail.com>
2023-03-23 18:34:48 -0700

    Task 2 and more prototype in main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiply the value of to numbers
+ *
+ * @a: number to be multiply
+ *
+ * @b: number to be multiply
+ *
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
